35fa97dcfa5899dc8376d1bd02a32eacf4b087ab
perillaroc <perillaroc@gmail.com>
2018-09-17 11:33:43 +0800

    move some routing utils into routing_util.py.

--- nmp_broker/__init__.py
+++ nmp_broker/__init__.py
@@ -1,35 +1,10 @@
 # coding=utf-8
-from datetime import datetime, time, timedelta, date
 from pathlib import Path
 
 from flask import Flask
-from flask.json import JSONEncoder
-from werkzeug.routing import BaseConverter, ValidationError
 
-from nmp_broker.common.config import load_config
-
-
-class NwpcMonitorBrokerApiJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.strftime("%Y-%m-%dT%H:%M:%S")
-        elif isinstance(obj, date):
-            return obj.strftime('%Y-%m-%d')
-        elif isinstance(obj, time):
-            return obj.strftime('%H:%M:%S')
-        elif isinstance(obj, timedelta):
-            return {'day': obj.days, 'seconds': obj.seconds}
-        return JSONEncoder.default(self, obj)
-
-
-class NoStaticConverter(BaseConverter):
-    def to_python(self, value):
-        if value == 'static':
-            raise ValidationError()
-        return value
-
-    def to_url(self, value):
-        return str(value)
+from .common.config import load_config
+from .common.routing_util import NMPBrokerApiJSONEncoder, NoStaticConverter
 
 
 def create_app(config_file_path=None):
@@ -41,7 +16,7 @@ def create_app(config_file_path=None):
                 template_folder=template_folder)
 
     app.config.from_object(load_config(config_file_path))
-    app.json_encoder = NwpcMonitorBrokerApiJSONEncoder
+    app.json_encoder = NMPBrokerApiJSONEncoder
     app.url_map.converters['no_static'] = NoStaticConverter
 
     with app.app_context():
---
+++ nmp_broker/common/routing_util.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+from datetime import datetime, time, timedelta, date
+
+from flask.json import JSONEncoder
+from werkzeug.routing import BaseConverter, ValidationError
+
+
+class NMPBrokerApiJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.strftime("%Y-%m-%dT%H:%M:%S")
+        elif isinstance(obj, date):
+            return obj.strftime('%Y-%m-%d')
+        elif isinstance(obj, time):
+            return obj.strftime('%H:%M:%S')
+        elif isinstance(obj, timedelta):
+            return {'day': obj.days, 'seconds': obj.seconds}
+        return JSONEncoder.default(self, obj)
+
+
+class NoStaticConverter(BaseConverter):
+    def to_python(self, value):
+        if value == 'static':
+            raise ValidationError()
+        return value
+
+    def to_url(self, value):
+        return str(value)
