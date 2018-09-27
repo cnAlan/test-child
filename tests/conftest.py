1bba56e2e46b6a8933d7543ca9899eb96d440f98
perillaroc <perillaroc@gmail.com>
2018-09-27 14:42:25 +0800

    :white_check_mark: add tests for workflow api v2.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,10 @@ from nmp_broker import create_app
 def app():
     app = create_app()
     yield app
+
+
+@pytest.fixture(scope="session")
+def client():
+    app = create_app()
+    client = app.test_client()
+    yield client
