7de6ef4c9167b33ffd275625b54fb4e830cf69f2
perillaroc <perillaroc@gmail.com>
2018-09-28 09:09:29 +0800

    :fire: delete api v2 fro sms.
    We use workflow for both sms and ecflow now.

--- nmp_broker/api_v2/__init__.py
+++ nmp_broker/api_v2/__init__.py
@@ -8,4 +8,3 @@ import nmp_broker.api_v2.api_repo
 import nmp_broker.api_v2.api_hpc
 import nmp_broker.api_v2.api_nofitication
 import nmp_broker.api_v2.api_workflow
-import nmp_broker.api_v2.api_sms
